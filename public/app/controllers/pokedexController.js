bf75eaace50fe120a88efc61ddee84be6c072062
Maggie Ng <iamtaiching@gmail.com>
2016-06-01 16:28:23 -0700

    modify the search function

--- public/app/controllers/pokedexController.js
+++ public/app/controllers/pokedexController.js
@@ -2,5 +2,13 @@ pokedex.controller('PokedexController', function($log, PokedexService) {
     var pokedex = this;
 
     pokedex.imgUrl = "assets/img/";
+
     pokedex.list = PokedexService.getAll();
+
+    pokedex.filterFunction = function(searchText){
+        return function(item){
+            var patt = new RegExp(searchText, "i");
+            return patt.test(item.name) || patt.test(item.id) || patt.test(item.type); //search for name, id and type only
+        }
+    }
 });
