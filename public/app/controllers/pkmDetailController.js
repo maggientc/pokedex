b8f14ed9c2089eb58777917c388afd70e9e71121
Maggie Ng <iamtaiching@gmail.com>
2016-06-04 21:25:20 -0700

    redirect to index page if no pkm data find

--- public/app/controllers/pkmDetailController.js
+++ public/app/controllers/pkmDetailController.js
@@ -1,5 +1,9 @@
-pokedex.controller('PkmDetailController', function($log, $stateParams, PokedexService) {
+pokedex.controller('PkmDetailController', function($log, $stateParams, $location, PokedexService) {
     var pkm = this;
     pkm.imgUrl = PokedexService.imgUrl;
     pkm.data = PokedexService.getByName($stateParams.pkmName);
+
+    if(!pkm.data){
+        $location.path('/');
+    }
 });
